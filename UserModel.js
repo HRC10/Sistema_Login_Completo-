0310f96e3e8a7b80bb17092b2c0863c49abe52e2
HRC10 <hugo.castor15@gmail.com>
2025-09-04 10:02:32 -0400

    Criando o modelo de usuário

---
+++ UserModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: [true, "Nome de usuário já em uso!!"],
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: [true, "E-mail já em uso!!"],
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
